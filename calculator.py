52959314231f53e08b72b5945416d300dc8c8951
Pushkar Singh <84075300+pushkar-pro@users.noreply.github.com>
2021-11-28 18:48:57 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -27,7 +27,7 @@ while True:
     print(num1, "*", num2, "=", multiply(num1, num2))
  elif choice == '4':
     print(num1, "/", num2, "=", divide(num1, num2))
- elif choice <= '999999':
+ else:
     print("Plese Select Valid Choice")
  print("==========================================")
  choice2 = input("Do You Want To Continue Yes/No?")

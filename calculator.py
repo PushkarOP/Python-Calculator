83c3c9561209ba8e727b26091421fc3f2b582ae8
Pushkar Singh <84075300+pushkar-pro@users.noreply.github.com>
2021-11-28 17:12:40 +0530

    update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,4 +1,31 @@
+def sum(x, y):
+       return x+y
+       
+def subtract(x, y):
+        return x-y
+ 
+def multiply(x, y):    
+       return x*y
+  
+def divide(x, y):  
+       return x/y
+       
+print("Welcome To Python Calculator\n=======================")
+print("Select operation.")
+print("1.Add")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
+choice = input("Enter choice(1/2/3/4): ")
+num1 = int(input("Enter The First Value :"))
+num2 = int(input("Enter The Second Value :"))
+if choice == '1':
+    print(num1, "+", num2, "=", sum(num1, num2))
+elif choice == '2':
+    print(num1, "-", num2, "=", subtract(num1, num2))
+elif choice == '3':
+    print(num1, "*", num2, "=", multiply(num1, num2))
+elif choice == '4':
+    print(num1, "/", num2, "=", divide(num1, num2))
 else:
-else:
-else:
-    print("Plese Select Correct Choice")
+    print("Plese Select Correct Choice.")

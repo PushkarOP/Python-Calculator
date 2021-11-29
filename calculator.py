add113e0c91b303c8d5129326b4ac3ea0b52aed2
Pushkar Singh <84075300+pushkar-pro@users.noreply.github.com>
2021-11-29 07:42:54 +0530

    update calculator.py

--- calculator.py
+++ calculator.py
@@ -9,6 +9,16 @@ def multiply(x, y):
   
 def divide(x, y):  
        return x/y
+
+def exponent(x, y):
+       return x**y
+
+def dividee(x, y):
+       return x//y
+
+def remainder(x, y):
+       return x%y
+
 while True:      
  print("Welcome To Python Calculator\n==========================================")
  print("Select operation.")
@@ -16,7 +26,10 @@ while True:
  print("2.Subtract")
  print("3.Multiply")
  print("4.Divide")
- choice = input("Enter choice(1/2/3/4): ")
+ print("5.Exponent")
+ print("6.Floor Divide")
+ print("7.Remainder")
+ choice = input("Enter choice(1/2/3/4/5/6/7): ")
  num1 = int(input("Enter The First Value :"))
  num2 = int(input("Enter The Second Value :"))
  if choice == '1':
@@ -27,14 +40,17 @@ while True:
     print(num1, "*", num2, "=", multiply(num1, num2))
  elif choice == '4':
     print(num1, "/", num2, "=", divide(num1, num2))
+ elif choice == '5':
+    print(num1, "**", num2, "=", exponent(num1, num2))
+ elif choice == '6':
+    print(num1, "//", num2, "=", dividee(num1, num2))
+ elif choice == '7':
+    print(num1, "%", num2, "=", remainder(num1, num2))
  else:
-    print("Plese Select Valid Choice")
+    print("Plaese Select Correct Choice")
  print("==========================================")
  choice2 = input("Do You Want To Continue Yes/No?")
- if choice2 == "No":
+ if choice2 in ("No", "no") :
    print("Thanks For Using Calculator")
-   break 
- elif choice2 == "no":
-   print("Thanks For Using Calculator")
-   break 
+   break
  print("==========================================")
